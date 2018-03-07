e52af969faee6a567b7b72e936473c0ec5bdadbd
wnagzihxa1n <wangzhixian618@gmail.com>
2018-03-07 21:56:57 +0800

    Update
    添加文章《一步一步学写Android壳---壳代码移植Native层》

---
+++ wnagzihxa1n/Android/一步一步学写Android壳/6.壳代码移植Native层/ProtectAPK/app/src/main/cpp/native-lib.h
@@ -0,0 +1,26 @@
+//
+// Created by wangz on 2018/3/6 0006.
+//
+
+#include <jni.h>
+#include <string>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <android/log.h>
+#include <assert.h>
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+
+#ifndef PROTECTAPK_NATIVE_LIB_H
+#define PROTECTAPK_NATIVE_LIB_H
+
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "toT0C",__VA_ARGS__) // 定义LOGI类型
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "toT0C" ,__VA_ARGS__) // 定义LOGE类型
+
+#endif //PROTECTAPK_NATIVE_LIB_H
+
